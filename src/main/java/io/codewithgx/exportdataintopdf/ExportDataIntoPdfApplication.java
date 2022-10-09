b297cddf4bb3344c5637bd2b86d0748c62e1d440
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2022-10-09 23:28:25 +0100

    Initial commit

---
+++ src/main/java/io/codewithgx/exportdataintopdf/ExportDataIntoPdfApplication.java
@@ -0,0 +1,13 @@
+package io.codewithgx.exportdataintopdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExportDataIntoPdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExportDataIntoPdfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/codewithgx/exportdataintopdf/ExportDataIntoPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codewithgx.exportdataintopdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExportDataIntoPdfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

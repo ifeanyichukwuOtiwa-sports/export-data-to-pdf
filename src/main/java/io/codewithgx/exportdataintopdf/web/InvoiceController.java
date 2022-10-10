9091bf4b6ba6f70eb916bd2eb750c9dc5c6185c4
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2022-10-10 01:07:08 +0100

    adds web Layer

---
+++ src/main/java/io/codewithgx/exportdataintopdf/web/InvoiceController.java
@@ -0,0 +1,95 @@
+package io.codewithgx.exportdataintopdf.web;
+
+import java.util.List;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import io.codewithgx.exportdataintopdf.custom.InvoiceNotFoundException;
+import io.codewithgx.exportdataintopdf.entity.Invoice;
+import io.codewithgx.exportdataintopdf.service.api.InvoiceServiceApi;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Created by @author Ifeanyichukwu Otiwa
+ * 10/10/2022
+ */
+
+
+@RestController
+@RequestMapping({"/api/v1/invoice", "/invoice"})
+@RequiredArgsConstructor
+public class InvoiceController {
+
+    private final InvoiceServiceApi invoiceServiceApi;
+
+    @GetMapping("/")
+    public String showHomepage() {
+        return "home";
+    }
+
+    @GetMapping("/register")
+    public String registration(){
+        return "register";
+    }
+
+    @PostMapping("/save")
+    public String saveInvoice(@ModelAttribute Invoice invoice, Model model) {
+        final Invoice savedInvoice = invoiceServiceApi.saveInvoice(invoice);
+        String message = "Record " + savedInvoice.toString() + " is saved successfully!";
+        model.addAttribute("message", message);
+        return "register";
+    }
+
+    @GetMapping("/getAllInvoice")
+    public String getAllInvoice(@RequestParam(value = "message", required = false) String message, Model model) {
+        final List<Invoice> allInvoices = invoiceServiceApi.getAllInvoices();
+        model.addAttribute("list", allInvoices);
+        model.addAttribute("message", message);
+        return "allInvoices";
+    }
+
+    @GetMapping("/edit")
+    public String getEditPage(Model model, RedirectAttributes redirectAttributes, @RequestParam Long id) {
+        String page = "";
+        try {
+            Invoice invoice = invoiceServiceApi.getInvoiceById(id);
+            model.addAttribute("invoice", invoice);
+            page = "editInvoice";
+        } catch (InvoiceNotFoundException e) {
+            e.printStackTrace();
+            redirectAttributes.addAttribute("message", e.getMessage());
+            page = "redirect:getAllInvoices";
+        }
+        return page;
+    }
+
+    @PostMapping("/update")
+    public String updateInvoice(@ModelAttribute("invoice") Invoice invoice, RedirectAttributes redirectAttributes) {
+        invoiceServiceApi.updateInvoice(invoice);
+        Long id = invoice.getId();
+        redirectAttributes.addAttribute("message", "Invoice with id: '%d' is updated successfully !".formatted(id));
+        return "redirect:getAllInvoices";
+    }
+
+    @GetMapping("/delete")
+    public String deleteInvoice(
+            @RequestParam Long id,
+            RedirectAttributes attributes
+    ) {
+        try {
+            invoiceServiceApi.deleteInvoiceById(id);
+            attributes.addAttribute("message", "Invoice with Id : '%d' is removed successfully!".formatted(id));
+        } catch (Exception e) {
+            e.printStackTrace();
+            attributes.addAttribute("message", e.getMessage());
+        }
+        return "redirect:getAllInvoices";
+    }
+}
